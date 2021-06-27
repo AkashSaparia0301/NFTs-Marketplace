06ce8eae0a199ff2e70cd9c4c71d307edb5cb542
Akash Saparia <37653697+AkashSaparia0301@users.noreply.github.com>
2021-06-27 20:05:17 +0530

    NFT Marketplace

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './footer.module.scss';
+
+
+const Footer = () => (
+  <footer className={styles.footer}>
+    <div className={styles.brand}>
+      <div className={styles.created}>
+        Created by DouBLe ViSioN
+      </div>
+    </div>
+  </footer>
+)
+
+export default Footer;
